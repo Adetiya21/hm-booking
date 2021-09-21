ae59231519499022f87724f7ddd2a389e35aef0b
Adetiya21 <adetiyaburhasanp45@gmail.com>
2021-09-21 09:39:50 +0700

    v1

--- application/views/utama/temp-footer.php
+++ application/views/utama/temp-footer.php
@@ -22,9 +22,9 @@
         <div class="col-lg-4 col-md-4 col-sm-6">
           <div class="footer__widget">
             <div class="footer__social">
-              <a href="<?= $ten->fb ?>"><i class="fa fa-facebook"></i></a>
-              <a href="<?= $ten->ig ?>"><i class="fa fa-instagram"></i></a>
-              <a href="tel:<?= $ten->no_telp ?>"><i class="fa fa-phone"></i></a>
+              <a href="htps://instagram.com/<?= $ten->fb ?>"><i class="fa fa-facebook"></i></a>
+              <a href="htps://facebook.com/<?= $ten->ig ?>"><i class="fa fa-instagram"></i></a>
+              <a href="https://wa.me/<?= $ten->no_telp ?>"><i class="fa fa-phone"></i></a>
             </div>
           </div>
         </div>
