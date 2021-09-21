eafa5f73785f34cd9969c7776d6b499ab1fccb6b
Adetiya21 <adetiyaburhasanp45@gmail.com>
2021-09-21 15:40:04 +0700

    fixed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.1.4/hm-booking/';
+$config['base_url'] = 'http://localhost/hm-booking/';
 
 /*
 |--------------------------------------------------------------------------
